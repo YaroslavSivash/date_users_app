82457f922eaecb370d0d1903f1a76c1150363bd4
UDT-dev5 <syvash.y@udtech.global>
2021-08-10 15:01:24 +0300

    10.1

--- user/repository.go
+++ user/repository.go
@@ -1,1 +1,12 @@
 package user
+
+import (
+	"context"
+	"date_users_app/models"
+)
+
+type UserRepository interface {
+	CreateUser (ctx context.Context, user *models.User, Email string) error
+	GetUsers (ctx context.Context, user *models.User) error
+	UpdateUser (ctx context.Context, user *models.User) (Id string, error)
+}
--- user/repository/mongo/user.go
+++ user/repository/mongo/user.go
@@ -1,1 +1,36 @@
 package mongo
+
+import "date_users_app/models"
+
+type User struct {
+	Id         primitive.ObjectID `bson:"_id,omitempty"`
+	Email      string  `bson:"email"`
+	LastName   string `bson:"lastname"`
+	Country    string `bson:"country"`
+	City       string `bson:"city"`
+	Gender     string `bson:"gender"`
+	Birth_date string `bson:"birth_date"`
+}
+
+
+type UserRepository struct {
+	db *mongo.Collection
+}
+
+func toMongoUser(u *models.User) *User {
+	return &User{
+		Email: u.Email,
+	}
+}
+
+func toModel(u *User) *models.User {
+	return &models.User{
+		Id:         u.Id.Hex(),
+		Email:      u.Email,
+		LastName:   u.LastName,
+		Country:    u.Country,
+		City:       u.City,
+		Gender:     u.Gender,
+		Birth_date: u.Birth_date,
+	}
+}
